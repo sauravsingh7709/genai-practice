fa98d86d906a29c0ff29ac224f10c56866172759
saurav_7709 <shailendrakumarsingh13316@gmail.com>
2026-04-17 13:48:57 +0530

    implementing rag

---
+++ 05-rag-1/chat.py
@@ -0,0 +1,52 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from openai import OpenAI
+
+
+
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+client = OpenAI()
+
+embedding_model = OpenAIEmbeddings(
+    model="text-embedding-3-small", 
+)
+
+vector_db= QdrantVectorStore.from_existing_collection(
+    url="http://localhost:6333",
+    collection_name="vector_learning",
+    embedding=embedding_model
+)
+
+query = input("> ")
+
+search_results = vector_db.similarity_search(
+    query=query
+)
+
+context = "\n\n\n".join([f"Page Content: {result.page_content}\nPage Number: {result.metadata['page_label']}\nFile Location: {result.metadata['source']}" for result in search_results])
+
+SYSTEM_PROMPT = f"""
+    You are a helpfull AI Assistant who asnweres user query based on the available context
+    retrieved from a PDF file along with page_contents and page number.
+
+    You should only ans the user based on the following context and navigate the user
+    to open the right page number to know more.
+
+    Context:
+    {context}
+"""
+
+chat_completion = client.chat.completions.create(
+    model="gpt-4.1",
+    messages=[
+        { "role": "system", "content": SYSTEM_PROMPT },
+        { "role": "user", "content": query },
+    ]
+)
+
+print(f"🤖: {chat_completion.choices[0].message.content}")
---
+++ 05-rag-1/main.py
@@ -0,0 +1,41 @@
+from langchain_community.document_loaders import PyPDFLoader
+from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+import os
+
+
+
+
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
+pdf_path=Path(__file__).parent/"nodejs.pdf"
+
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load()
+
+print (f"Total pages: {len(docs)}")
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200,
+)
+
+split_docs = text_splitter.split_documents(docs)
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+   
+)
+
+vector_store =QdrantVectorStore.from_documents(
+    documents=split_docs,
+    collection_name="vector_learning",
+    url="http://localhost:6333",
+    embedding=embeddings,
+)
+
+print("Documents added to vector store successfully!")
